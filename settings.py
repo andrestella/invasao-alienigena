2c76b1553c972c41175f6d487fde9511cb15fd20
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-03-01 15:53:00 -0300

    permitindo um movimento contínuo

--- settings.py
+++ settings.py
@@ -6,5 +6,5 @@ class Settings():
         """ Inicializa as configurações do jogo. """
         # Configurações da tela
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 750
         self.bg_color = (230, 230, 230)
