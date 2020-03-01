0953d697f6637ccf1e9bbe3b3474a6acd19177bc
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-03-01 18:29:10 -0300

    movendo tanto para a esquerda quanto para a direita

--- game_functions.py
+++ game_functions.py
@@ -9,9 +9,16 @@ def check_events(ship):
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        # Podemos usar blocos 'elif' nesse caso, pois cada evento está 
+        # associado a apenas uma tecla. Se o jogador pressionar as duas 
+        # teclas ao mesmo tempo, dois eventos diferentes serão detectados.
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
     """ Atualiza as imagens na tela e alterna para a nova tela. """
--- ship.py
+++ ship.py
@@ -15,13 +15,24 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        # Flag de movimento
+        # Flags de movimento
         self.moving_right = False
+        self.moving_left = False
+
 
     def update(self):
-        """ Atualiza a posição da espaçonave de acordo com a flag de movimento."""
+        """ Atualiza a posição da espaçonave de acordo com as flags de movimento."""
         if self.moving_right:
-            self.rect.centerx += 1
+            self.rect.centerx += 1   
+        if self.moving_left:
+            self.rect.centerx -= 1
+        # Usamos 'if' em vez de utilizar 'elif' para permitir que o valor rect.centerx
+        # seja incrementado e decrementado se as duas teclas forem mantidas pressionadas.
+        # Isso resulta na espaçonave parada. Se usássemos 'elif' para o movimento à
+        # esquerda, a seta para a direita sempre teria prioridade. Fazer isso dessa 
+        # maneira deixa os movimentos mais precisos ao alternarmos o movimento da 
+        # esquerda para a direita, quando o jogador poderia momentaneamente manter
+        # as duas teclas pressionadas.
 
     def blitme(self):
         """ Desenha a espaçonave em sua posição atual. """
