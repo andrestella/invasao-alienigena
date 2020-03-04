529fe46bc93648f8e33fdec184ba5978f29f0345
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-03-04 20:13:10 -0300

    desenhando o foguete na tela

--- exercicios/rocket.py
+++ exercicios/rocket.py
@@ -1,6 +1,35 @@
+import sys
 import pygame
 
+class Rocket():
+
+    def __init__(self, screen):
+        self.screen = screen
+        self.image = pygame.image.load("rocket.png")
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.center = self.screen_rect.center
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
+
 def run_game():
-    pass
+    pygame.init()
+    screen = pygame.display.set_mode((600, 600))
+    pygame.display.set_caption("Rocket")
+
+    bg_color = (255, 255, 255)
+
+    rocket = Rocket(screen)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.fill(bg_color)
+        rocket.blitme()
+        pygame.display.flip()
+        
 
 run_game()
