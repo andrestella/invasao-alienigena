ca136afc911f358664a0ffb426cc832ae45db63f
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-03-13 21:34:55 -0300

    criando configurações para a direção da frota

--- settings.py
+++ settings.py
@@ -21,3 +21,6 @@ class Settings():
 
         # Configurações dos alienígenas
         self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction igual a 1 representa a direita; -1 representa a esquerda
+        self.fleet_direction = 1
